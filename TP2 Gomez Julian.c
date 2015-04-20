fda2933a6f9aef6130be4f0b000d964a1682fca4
JulianGomez <juulian_gomez@yahoo.com.ar>
2015-04-19 21:16:46 -0300

    TP2- Commit 19/04
    Cambio en una variable

--- TP2 Gomez Julian.c
+++ TP2 Gomez Julian.c
@@ -1,3 +1,7 @@
+/* Gomez Julian 153.605-9
+    20150419
+*/
+
 #include <stdio.h>  
  
 int main()   
@@ -6,32 +10,32 @@ int main()
 printf("\nIntroduzca una cadena, EOF para terminar:\n");
  
 int c;
-int letra = 1; 
+int caracterEspecial= 0; 
 
 while ( (c = getchar()) !=  EOF )
 {   
 
-letra = 1;
+caracterEspecial= 0;
 
 if (c == '\t')
 {
 printf("\\t");
-letra = 0;
+caracterEspecial= 1;
 }
 
 if (c == '\b')
 {
 printf("\\b");
-letra = 0;
+caracterEspecial= 1;
 }
 
 if (c == '\\')
 {
 printf("\\");
-letra = 0;
+caracterEspecial= 1;
 }
 
-if (letra == 1)
+if caracterEspecial== 0)
 {
 printf("%c",c);
 }
